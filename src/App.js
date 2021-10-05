49baa8f7e1d1ef18f42125150fa4aaa4bd099db7
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-10-06 01:20:07 +0600

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Home from './components/home/Home';
+// import Home from './components/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import initializeAuthentication from './Firebase/Firebase.initialize';
+// initializeAuthentication()
+// const provider = new GoogleAuthProvider();
+import React, { Suspense } from 'react';
+
+const Home = React.lazy(() => import('./components/home/Home'));
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Suspense fallback={<div>loading...</div>}>
+        <Home></Home>
+      </Suspense>
+
     </div>
   );
 }
---
+++ src/Firebase/Firebase.Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvVUSlhbvxrDeu7hMDhRrK1Nbr2tIoAwU",
+    authDomain: "thematic-keel-310909.firebaseapp.com",
+    projectId: "thematic-keel-310909",
+    storageBucket: "thematic-keel-310909.appspot.com",
+    messagingSenderId: "836640188230",
+    appId: "1:836640188230:web:5713cfa028e8b7c63a2747",
+    measurementId: "G-FYYMFZGJNG"
+};
+export default firebaseConfig;
